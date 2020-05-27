167722c397347ba31ea02410d07690a2d99c7a4c
Amanda Key <ariaandezrasmom@gmail.com>
2020-05-27 13:17:52 -0500

    finally got button to work

--- src/App.js
+++ src/App.js
@@ -3,41 +3,55 @@ import Dog from "./components/Dog";
 import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  let dogs = [
-    {
-      name: "Scruffles",
-      type: "Chihuahua",
-    },
-    {
-      name: "Snarf",
-      type: "Border Collie",
-    },
-    {
-      name: "Wookie",
-      type: "English Sheepdog",
-    },
-  ];
+class App extends React.Component {
 
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
+  state = {
+    dogs: [
+      {
+        name: "Scruffles",
+        type: "Chihuahua",
+      },
+      {
+        name: "Snarf",
+        type: "Border Collie",
+      },
+      {
+        name: "Wookie",
+        type: "English Sheepdog",
+      },
+    ],
+    favoriteDog: "",
+  };
+
+  pickNewFavorite = () => {
+    let newDogIndex = Math.floor(Math.random() * this.state.dogs.length);
+    return this.state.dogs[newDogIndex];
+  }
+  completePickNewFavorite = () => {
+    this.setState({ favoriteDog: this.pickNewFavorite()})
   }
 
-  let favoriteDog = pickNewFavorite();
+  componentDidMount() {
+    this.setState({ favoriteDog: this.pickNewFavorite() });
+  }
 
-  return (
-    <div className="dogs">
-      <Header numDogs={dogs.length} />
-      <h2>My Dogs</h2>
-      <div>
-        {dogs.map((dog) => (
-          <Dog dog={dog} />
-        ))}
+  render() {
+    return (
+      <div className="dogs">
+        <Header numDogs={this.state.dogs.length} />
+        <h2>My Dogs</h2>
+        <div>
+          {this.state.dogs.map((dog) => (
+            <Dog dog={dog} />
+          ))}
+        </div>
+        <h2>My Favorite dog is {this.state.favoriteDog.name}</h2>
+        <button onClick={this.completePickNewFavorite} class="new__fav__btn">
+          Pick New Favorite
+        </button>
       </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
