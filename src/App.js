2439aaef548ddb7b7caed46a312ed8de33bc5b46
Jessica Thurman <jaithurman@gmail.com>
2020-05-26 22:19:15 -0500

    first dog

--- src/App.js
+++ src/App.js
@@ -3,41 +3,48 @@ import Dog from "./components/Dog";
 import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  let dogs = [
-    {
-      name: "Scruffles",
-      type: "Chihuahua",
-    },
-    {
-      name: "Snarf",
-      type: "Border Collie",
-    },
-    {
-      name: "Wookie",
-      type: "English Sheepdog",
-    },
-  ];
-
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
-  }
-
-  let favoriteDog = pickNewFavorite();
-
-  return (
-    <div className="dogs">
-      <Header numDogs={dogs.length} />
+class App extends React.Component{
+  state = {
+     dogs: [
+          {
+            name: "Scruffles",
+            type: "Chihuahua",
+          },
+          {
+            name: "Snarf",
+            type: "Border Collie",
+          },
+          {
+            name: "Wookie",
+            type: "English Sheepdog",
+          },
+        ],
+        favoriteDog: "",
+  };
+   pickNewFavorite() {
+        let newDogIndex = Math.floor(Math.random() * this.state.dogs.length);
+        return this.state.dogs[newDogIndex];
+      }
+      componentDidMount(){
+        this.setState({favoriteDog: this.pickNewFavorite()});
+      }
+      onPickNewFavorite = () => {
+        this.setState({favoriteDog: this.pickNewFavorite()})
+      }
+  render(){
+    return(
+      <div className="dogs">
+      <Header numDogs={this.state.dogs.length} />
       <h2>My Dogs</h2>
       <div>
-        {dogs.map((dog) => (
+        {this.state.dogs.map((dog) => (
           <Dog dog={dog} />
-        ))}
+          ))}
       </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
+      <h2>My Favorite dog is {this.state.favoriteDog.name}</h2>
+  <button type="button" onClick={this.onPickNewFavorite}> New Favorite</button>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
