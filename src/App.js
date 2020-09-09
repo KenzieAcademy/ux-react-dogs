5c19f8c62b2e37726b2dc6febd1dc343d7d5016d
beckner-uie <petitepaladin@gmail.com>
2020-09-08 22:53:52 -0400

    finished assignment

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Dog from "./components/Dog";
 import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  let dogs = [
+class App extends React.Component{
+  state = {
+    dogs: [
     {
       name: "Scruffles",
       type: "Chihuahua",
@@ -17,27 +18,31 @@ function App() {
       name: "Wookie",
       type: "English Sheepdog",
     },
-  ];
-
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
-  }
-
-  let favoriteDog = pickNewFavorite();
-
+  ],
+ favoriteDog: ""
+}
+pickNewFavorite() {
+  let newDogIndex = Math.floor(Math.random() * this.state.dogs.length);
+  return this.state.dogs[newDogIndex];
+}
+onPickNewFavorite = () => {
+  this.setState({favoriteDog: this.pickNewFavorite()})
+}
+  
+render() {
   return (
     <div className="dogs">
-      <Header numDogs={dogs.length} />
+      <Header numDogs={this.state.dogs.length} />
       <h2>My Dogs</h2>
       <div>
-        {dogs.map((dog) => (
+        {this.state.dogs.map((dog) => (
           <Dog dog={dog} />
         ))}
       </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
+      <h2>My Favorite dog is {this.state.favoriteDog.name}</h2>
+    <button onClick={this.onPickNewFavorite}>Pick New Favorite</button>
     </div>
-  );
+  );}
 }
 
 export default App;
